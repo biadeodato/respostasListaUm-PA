1fad50fa51d650499313351851d20005a3d5cd1c
Beatriz <bia.deodato8787@gmail.com>
2024-05-13 18:11:01 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,40 @@
+package lista;
+
+public class Produto {
+private String nome;
+private double preco;
+private int quantidade;
+
+public Produto(String n, double p, int q) {
+	this.nome = n;
+	this.preco = p;
+	this.quantidade = q;
+	
+}
+public String getNome() {
+	return nome;
+	
+}
+public double getPreco() {
+	return preco;
+	
+}
+public int getQuantidade() {
+	return quantidade;
+}
+public void setNome(String novoNome) {
+	this.nome = novoNome;
+}
+
+public void setPreco(double novoPreco) {
+	this.preco = novoPreco;
+}
+public void setQuantidade(int novaQuantidade) {
+	this.quantidade = novaQuantidade;
+}
+public double CalcularTotal() {
+	return this.preco * this.quantidade;
+	
+}
+
+}
