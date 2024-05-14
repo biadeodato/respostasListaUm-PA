a88ad1474698a255084c2b10919c2a76b2d87e79
Beatriz <bia.deodato8787@gmail.com>
2024-05-14 16:41:53 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,37 @@
+package lista;
+
+public class Cachorro {
+private String nome;
+private String raca;
+private int idade;
+
+public Cachorro(String n, String r, int i) {
+	this.nome = n;
+	this.raca = r;
+	this.idade = i;
+}
+public String getNome() {
+	return nome;
+}
+public String getRaca() {
+	return raca;
+}
+public int getIdade() {
+	return idade;
+}
+public void setNome(String novoNome) {
+	this.nome = novoNome;
+}
+public void setRaca(String novaRaca) {
+this.raca = novaRaca;	
+}
+public void setIdade(int novaIdade) {
+	this.idade = novaIdade;
+}
+public String latir() {
+	return "Au au";
+
+
+}
+
+}
