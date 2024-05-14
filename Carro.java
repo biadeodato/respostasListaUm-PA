384ca9b0ee7fec4e8c9e4f9178495f655cd28cf4
Beatriz <bia.deodato8787@gmail.com>
2024-05-14 16:40:22 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,43 @@
+package lista;
+
+public class Carro {
+    private String modelo;
+    private String marca;
+    private int ano;
+    private String placa;
+     
+    public Carro(String m, String ma, int a, String p) {
+    	this.modelo = m;
+    	this.marca = ma;
+    	this.ano = a;
+    	this.placa = p;
+    }
+     
+    public String getModelo() {
+    	return modelo;
+    	
+    }
+    public String getMarca() {
+    	return marca;
+    }
+   public int getAno() {
+	   return ano;
+   }
+   public String getPlaca() {
+	   return placa;
+   }
+   public void setModelo(String novoModelo) {
+	   this.modelo = novoModelo;
+   }
+   public void setMarca(String novaMarca) {
+	   this.marca = novaMarca;
+   }
+   public void setAno(int novoAno) {
+	   this.ano = novoAno;
+	   
+   }
+   public void setPlaca(String novaPlaca) {
+	   
+   }
+   
+}
