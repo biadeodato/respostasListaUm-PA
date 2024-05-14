58b71f13760da608b9c213fee91617c920906b9f
Beatriz <bia.deodato8787@gmail.com>
2024-05-14 16:35:41 -0300

    Create Hora.java

---
+++ Hora.java
@@ -0,0 +1,42 @@
+package lista;
+
+public class Pessoa {
+ private String nome;
+ private int idade;
+ private double altura;
+
+ public Pessoa(String n, int i, double a) {
+	 this.nome = n;
+	 this.idade = i;
+	 this.altura = a;
+	 
+ }
+  public String getNome() {
+	  return nome;
+	 
+  }
+  public int getIdade() {
+	  return idade;
+	 
+  }
+  public double getAltura() {
+	  return altura;
+	 
+  }
+  public void setNome(String novoNome) {
+	  this.nome = novoNome;
+	  
+  }
+  public void setIdade(int novaIdade) {
+	  this.idade = novaIdade;
+	  
+  }
+  public void setAltura(double novaAltura) {
+	this.altura = novaAltura;
+  }
+  public String toString() {
+  return "Nome:" + nome + "\nIdade:" + idade + "anos\nAltura" + altura + "m" ;
+  
+  }
+  
+}
